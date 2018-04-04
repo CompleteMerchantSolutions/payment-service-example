638bfc3626b5cea232f577028a5e0307f5f56cc2
Pete Anderson <me@bipvanwinkle.com>
2018-04-04 12:21:23 -0600

    Added script to retrieve OTUT

---
+++ get-one-time-use-token.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+const dotenv = require('dotenv');
+
+function getOneTimeUseToken() {
+    const testMerchantId = 888886
+    const gatewayName = 'usaepay' // Can also be set to "nmi"
+    return axios.post(
+        'https://api.emviodev.com/pay/v3/token',
+        {
+            merchantId: testMerchantId,
+            gateway: {
+                name: gatewayName
+            },
+            data: {
+                amount: 50.5
+            }
+        },
+        {
+            headers: {
+                Authorization: process.env.AUTH_TOKEN
+            }
+        }
+    );
+}
+
+dotenv.config()
+getOneTimeUseToken()
+    .then(({ data }) => {
+        console.log(data)
+    }).catch(err => {
+        if (err && err.data) {
+            console.error(err.data.message)
+        } else {
+            console.error(err)
+        }
+    })
