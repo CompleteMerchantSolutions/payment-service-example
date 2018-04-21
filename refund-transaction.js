a0f9d40fb778d2efde0562101284ee71bbffe378
evelynlopz <evelynl@codev.com>
2018-04-21 09:44:16 +0800

    Updated removed the use of cognito authentication.

--- refund-transaction.js
+++ refund-transaction.js
@@ -1,44 +1,9 @@
 const axios = require('axios');
 const dotenv = require('dotenv');
-const {
-    CognitoUserPool,
-    AuthenticationDetails,
-    CognitoUser
-} = require('amazon-cognito-identity-js-node');
 
 dotenv.config();
 
-function getJWT() {
-    const authenticationData = {
-        Username: process.env.API_USERNAME,
-        Password: process.env.PASSWORD
-    };
-    const authenticationDetails = new AuthenticationDetails(authenticationData);
-    const poolData = {
-        UserPoolId: process.env.USER_POOL_ID,
-        ClientId: process.env.CLIENT_ID
-    };
-    const userPool = new CognitoUserPool(poolData);
-    const userData = {
-        Username: authenticationData.Username,
-        Pool: userPool
-    };
-    const cognitoUser = new CognitoUser(userData);
-
-    return new Promise((resolve, reject) => {
-        cognitoUser.authenticateUser(authenticationDetails, {
-            onSuccess: function(result) {
-                resolve(result.idToken.jwtToken);
-            },
-            onFailure: function(err) {
-                reject(err);
-            }
-        });
-    });
-}
-
 async function runTransaction() {
-    const jwt = await getJWT();
     const postBody = {
         merchantId: process.env.MERCHANT_ID,
         data: {
@@ -51,7 +16,7 @@ async function runTransaction() {
     };
     const config = {
         headers: {
-            Authorization: jwt
+            Authorization: process.env.JWT
         }
     };
     const { data } = await axios.post(process.env.API_URL + 'pay/v3/refund', postBody, config);
@@ -60,13 +25,13 @@ async function runTransaction() {
 }
 
 runTransaction()
-    .then(transactionResponse => {
-        console.log(transactionResponse);
-    })
-    .catch(err => {
-        if (err && err.response) {
-            console.error(err.response.data.message);
-        } else {
-            console.error(err);
-        }
-    });
+.then(transactionResponse => {
+    console.log(transactionResponse);
+})
+.catch(err => {
+    if (err && err.response) {
+        console.error(err.response.data.message);
+    } else {
+        console.error(err);
+    }
+});
--- run-transaction.js
+++ run-transaction.js
@@ -1,44 +1,9 @@
 const axios = require('axios');
 const dotenv = require('dotenv');
-const {
-    CognitoUserPool,
-    AuthenticationDetails,
-    CognitoUser
-} = require('amazon-cognito-identity-js-node');
 
 dotenv.config();
 
-function getJWT() {
-    const authenticationData = {
-        Username: process.env.USERNAME,
-        Password: process.env.PASSWORD
-    };
-    const authenticationDetails = new AuthenticationDetails(authenticationData);
-    const poolData = {
-        UserPoolId: process.env.USER_POOL_ID,
-        ClientId: process.env.CLIENT_ID
-    };
-    const userPool = new CognitoUserPool(poolData);
-    const userData = {
-        Username: authenticationData.Username,
-        Pool: userPool
-    };
-    const cognitoUser = new CognitoUser(userData);
-
-    return new Promise((resolve, reject) => {
-        cognitoUser.authenticateUser(authenticationDetails, {
-            onSuccess: function(result) {
-                resolve(result.idToken.jwtToken);
-            },
-            onFailure: function(err) {
-                reject(err);
-            }
-        });
-    });
-}
-
 async function runTransaction() {
-    const jwt = await getJWT();
     const postBody = {
         merchantId: process.env.MERCHANT_ID,
         tokenex: {
@@ -57,7 +22,7 @@ async function runTransaction() {
     };
     const config = {
         headers: {
-            Authorization: jwt
+            Authorization: process.env.JWT
         }
     };
     const { data } = await axios.post(process.env.API_URL + 'pay/v3/process', postBody, config);
